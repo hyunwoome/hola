421cf9d41c0afaa48f8431881d98401475400a2f
hyunwoome <hyunwoome@icloud.com>
2023-06-09 22:31:56 +0900

    feat: typeorm config 연결

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,10 +1,10 @@
-export default () => ({
-  port: parseInt(process.env.PORT, 10),
-  database: {
-    host: process.env.MYSQL_HOST,
-    port: parseInt(process.env.MYSQL_PORT, 10),
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_ROOT_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
-  },
-});
+// export default () => ({
+//   port: parseInt(process.env.PORT, 10),
+//   database: {
+//     host: process.env.MYSQL_HOST,
+//     port: parseInt(process.env.MYSQL_PORT, 10),
+//     user: process.env.MYSQL_USER,
+//     password: process.env.MYSQL_ROOT_PASSWORD,
+//     database: process.env.MYSQL_DATABASE,
+//   },
+// });
